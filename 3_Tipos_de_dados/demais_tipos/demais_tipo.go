3ebe5c27915f2ae6843e18c6e4d3723ad5272022
rzjprogramador <rzjprogramador@gmail.com>
2022-12-05 14:35:25 -0300

    docs: [fundamentos] valores iniciais default

---
+++ 3_Tipos_de_dados/demais_tipos/demais_tipo.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var erro error
+var boleano bool
+var textoDefault string
+var numDefault float32
+
+func main() {
+	fmt.Println(erro)
+	fmt.Println(boleano)
+	fmt.Println(textoDefault)
+	fmt.Println(numDefault)
+}
