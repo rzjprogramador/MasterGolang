0ed8680a27c5d38e4408133e8c1ef0875e6792f0
rzjprogramador <rzjprogramador@gmail.com>
2022-12-06 14:13:24 -0300

    feat: objeto rigido map

---
+++ objRigidoMap/ObjRigidoMap.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	usuarioMap := map[string]string{
+		"nome":      "Reinaldo",
+		"sobrenome": "Zacharias",
+	}
+
+	fmt.Println(usuarioMap)
+}
+
+/*
+map é um objeto tipo json só que rigido.
+declaracao: nomedoMap := map[tipoKey]tipoValue { props}
+obs: todas as propriedades tem que ter virgula no final ... inclusive a ultima.
+
+ - as chaves tem que ser todas do mesmo tipo declarado.
+ - e os valores tem que ser todos do mesmo tipo declarado.
+ exemplo se passou que as chaves serao string todas as chaves terao que ser strings
+  se passou que os valores serao inteiro todas os valores terao que ser inteiros
+
+*/
