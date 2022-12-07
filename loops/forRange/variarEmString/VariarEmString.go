c351a16b6b50715946cc44c6b79aa8c8e6fdb27a
rzjprogramador <rzjprogramador@gmail.com>
2022-12-07 10:56:12 -0300

    refactor: okok

--- loops/forRange/variarEmString/VariarEmString.go
+++ loops/forRange/variarEmString/VariarEmString.go
@@ -8,12 +8,12 @@ func main() {
 
 func VariarEmString() {
 	texto := "TEXTO"
-	for indice, letra := range texto {
+	for posicao, letra := range texto {
 		// VAI PRINTAR EM TABELA ASC
-		// fmt.Println(indice, letra)
+		// fmt.Println(posicao, letra)
 
 		// VAI PRINTAR EM STRING
-		fmt.Println(indice, string(letra))
+		fmt.Println(posicao, string(letra))
 	}
 }
 
@@ -23,8 +23,17 @@ func VariarEmString() {
 
 significado: Variar Para ...
 
+sitaxe: for param1Posicao?, paramConteudoDoItem? := igual numeradoRange? numerarQuem coloca aqui a fonteAlvoDaNumeracao { faz isto
+	# usa os params recebidos que vao ficar passando em loop a cada iteracao
+  tenho na maoa posicao e o conteudoDoItem a cada iteracao aqui dentro
+	-> se der um print  nestes params vou ve-los no console
+	-> posso fazer o que quiser com eles.
+}
+
+consideracoes: o for é como uma funcao nomedoLoop paramPosicao1, paramConteudo2 := range alvoNumeracao { usa os params em loopACadaIteracao } 
+
 restricoes: NAO DÁ PARA FAZER FOR COM RANGE EM STRUCTS ESTRUTURAS.
 
 -  para ocultar a var que mostra o indice coloque um anderline no lugar.
 
- */
+*/
