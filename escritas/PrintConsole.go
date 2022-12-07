4400c11c6c633f29f02f8983a4512b8debf96f73
rzjprogramador <rzjprogramador@gmail.com>
2022-12-07 14:43:06 -0300

    feat: [escritas] print no console

---
+++ escritas/PrintConsole.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	texto := "Meu 1º texto"
+
+	// funcao print , sozinha só printa nao pula linha no console.
+	print(texto)
+
+	// funcao Println do obj fmt ela printa e pula linha no console
+	fmt.Println(texto)
+
+	// Printf() do obj fmt ... posso renderizar variaveis atraves da tag %s porcentagemTipo.
+	fmt.Printf("Meu texto é >> %s ...", texto)
+
+}
