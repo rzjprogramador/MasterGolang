980c7c83be302fb30a69f4e7527daae0e4339ab9
rzjprogramador <rzjprogramador@gmail.com>
2022-12-08 11:17:50 -0300

    feat: operadores unarios aumentar ou diminuir numeros

--- operadores/unarios/Unarios.go
+++ operadores/unarios/Unarios.go
@@ -6,14 +6,15 @@ func main() {
 	aumentar := 10
 	aumentar++
 	aumentar++
-	aumentar++
+	aumentar += 55
 
 	diminuir := 20
 	diminuir--
 	diminuir--
+	diminuir -= 10
 
-	fmt.Println(aumentar)
-	fmt.Println(diminuir)
+	fmt.Println(aumentar) // comecou com 10, aumentou 2 vezes de 1 deu 12, aumentou 55, total: 67
+	fmt.Println(diminuir) // comecou com 20, diminuiu 2 vezes de 1 deu 18, diminuiu -10 total: 8
 }
 
 /*
@@ -24,9 +25,15 @@ entrada:
 	existentes: sao eles ++ para aumentar , -- para diminuir
 
 processamento:
- acao: os encurtadores unarios aumentam ou diminuem de 1 em 1 unidade.
- abreviamos para numero++  ,  SERIA O MESMO DE numero = numero + 1 ,
- narracao: "numero é igual o que ela ja tem + mais 1"
+ acao: os encurtadores unarios aumentam ou diminuem :
+
+ narracao: "numero é igual o que ela ja tem + mais 1 ..ou -1 se for dinuir"
+
+ ++ aumenta de 1 em 1 unidade. // SERIA O MESMO DE numero = numero + 1 ,
+ -- diminui de 1 em 1 unidade // SERIA O MESMO DE numero = numero -1,
+ += 15  :: vai aumentar o que ja tem para mais 15
+ -= 33  :: vai diminuir o que tem por 33
+
 
 saida:
 ++ para aumentar , -- para diminuir
