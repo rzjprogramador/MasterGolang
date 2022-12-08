51d242c1335370a11348f4e75902d6fa33f218fa
rzjprogramador <rzjprogramador@gmail.com>
2022-12-08 10:33:29 -0300

    feat: operadores

--- operadores/Logicos.go
+++ operadores/Logicos.go
@@ -4,10 +4,16 @@ import "fmt"
 
 func main() {
 
-	verdadeiro, falso := true, false 
+	verdadeiro, falso, foo := true, false, "bar"
 
-	// OPERADORES LOGICOS ( && , || ) : SÓ RETORNAM BOLEANOS
-	
+	/*
+	 OPERADORES LOGICOS ( && , ||, ! ) :
+	 processamento: SÃO PERGUNTAS , É VERDADEIRO (true) ? É FALSO (false)?
+
+	 saida:
+	 ASSIM QUE ENCONTRAM A PRIMEIRA OCORRENCIA DO SEU OBJETIVO JÁ RETORNAM BOLEANOS TRUE OU FALSE -- E INTERROMPEM AS VERIFICACOES
+	*/
+
 	fmt.Println(verdadeiro && falso) // false
 
 	// operador_E : && todas tem que ser verdade pra retornar verdade , narracao: Esta "E" estas pra ser verdade.
@@ -15,5 +21,10 @@ func main() {
 	fmt.Println(verdadeiro || falso) // true
 
 	// operador_OU : || basta uma ser verdade pra retornar verdade, narracao: Pode ser Esta "OU" estas pra ser verdade.
+
+	fmt.Println(!verdadeiro)
+	// operador_NEGACAO : ! inverte o valor bolaeano de uma referencia, se era true vira false e vice e versa
+
+	fmt.Println(!!foo)
 
 }
