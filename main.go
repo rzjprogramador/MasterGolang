d8f89c6c453b20af2478f2f786ad70a3ee441f03
rzjprogramador <rzjprogramador@gmail.com>
2022-12-08 15:42:52 -0300

    refactor: okok

--- main.go
+++ main.go
@@ -1,35 +1,31 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/MasterGolang/conversoes"
-	"github.com/MasterGolang/escritas"
-	"github.com/MasterGolang/loops"
-	"github.com/MasterGolang/modules/math"
-	"github.com/MasterGolang/objetos"
-	"github.com/MasterGolang/operadores"
-	"github.com/MasterGolang/tiposDeDados"
-	"github.com/MasterGolang/variaveis"
-)
+// "github.com/MasterGolang/variaveis"
+// "github.com/MasterGolang/conversoes"
+// "github.com/MasterGolang/escritas"
+// "github.com/MasterGolang/loops"
+// "github.com/MasterGolang/modules/math"
+// "github.com/MasterGolang/objetos"
+// "github.com/MasterGolang/operadores"
+// "github.com/MasterGolang/tiposDeDados"
 
 func main() {
-	// print("Hello Word - Main")
-	variaveis.Declaracoes()
-	conversoes.StringParaNumero()
-	escritas.PrintConsole()
-	loops.Incrementar()
-	loops.PrintarItems()
-	loops.VariarEmString()
-	loops.VariarPorObjetoMap()
-	fmt.Println(math.Soma(10, 20))
-	objetos.StructModeloEstrutura()
-	objetos.HerancaEstrutura()
-	objetos.ObjRigidoMap()
-	operadores.Logicos()
-	operadores.Unarios()
-	tiposDeDados.Erro()
-	tiposDeDados.NumFloat()
-	tiposDeDados.NumInteiro()
-	tiposDeDados.ValoresIniciais()
+	print("**** Hello Word - Main **** \n")
+	// variaveis.Declaracoes()
+	// conversoes.StringParaNumero()
+	// escritas.PrintConsole()
+	// loops.Incrementar()
+	// loops.PrintarItems()
+	// loops.VariarEmString()
+	// loops.VariarPorObjetoMap()
+	// fmt.Println(math.Soma(10, 20))
+	// objetos.StructModeloEstrutura()
+	// objetos.HerancaEstrutura()
+	// objetos.ObjRigidoMap()
+	// operadores.Logicos()
+	// operadores.Unarios()
+	// tiposDeDados.Erro()
+	// tiposDeDados.NumFloat()
+	// tiposDeDados.NumInteiro()
+	// tiposDeDados.ValoresIniciais()
 }
