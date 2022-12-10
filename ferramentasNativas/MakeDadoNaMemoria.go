afe35772e1d4560bbcec0297a7e0ee6798cf8f70
rzjprogramador <rzjprogramador@gmail.com>
2022-12-10 12:48:21 -0300

    feat: [ferramentas] ferramentas golang

--- ferramentasNativas/MakeDadoNaMemoria.go
+++ ferramentasNativas/MakeDadoNaMemoria.go
@@ -6,13 +6,15 @@ func MakeDadoNaMemoria() {
 	criarSliceDeNumerosNaMemoria := make([]uint32, 5, 9)
 
 	fmt.Println(len(criarSliceDeNumerosNaMemoria)) 
-	// funcao len(alvo) :: funcao len() mostra o tamanho do alvo passado
-
+	
 	fmt.Println(cap(criarSliceDeNumerosNaMemoria)) 
-	// funcao cap(alvo) :: funcao cap mostra a capacidade o alvo passado 
 }
 
 /*
+// funcao len(alvo) :: funcao len() mostra o tamanho do alvo passado
+
+// funcao cap(alvo) :: funcao cap mostra a capacidade o alvo passado 
+
 funcao make(tipo, tamanho, capacidade)
 funcao make() cria qualquer dado na memoria.package ferramentasNativas
 
