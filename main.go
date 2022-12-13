2ee0ce2aa45268c4d59a3a4383651a39e7302320
rzjprogramador <rzjprogramador@gmail.com>
2022-12-13 14:36:01 -0300

    docs: [erros] erros aleatorios nao_usado declaracoes nao usadas.

--- main.go
+++ main.go
@@ -1,65 +1,81 @@
 package main
 
-import (
-	// "log"
+// import (
+// 	// "log"
 
-	// "github.com/MasterGolang/ferramentasFuncoes"
-	// "github.com/MasterGolang/ferramentasNativas"
-	// "github.com/MasterGolang/ferramentasTecnicas"
-	// "github.com/MasterGolang/ferramentasNativas"
-	// "github.com/MasterGolang/ferramentasTecnicas"
-	// "github.com/MasterGolang/funcoes"
-	// "github.com/MasterGolang/Base/agrupamento"
-	// "github.com/MasterGolang/Base/conversoes"
-	// "github.com/MasterGolang/Base/escritas"
-	// "github.com/MasterGolang/Base/fluxo"
-	// "github.com/MasterGolang/Base/loops"
-	// "github.com/MasterGolang/Base/objetos"
-	// "github.com/MasterGolang/Base/operadores"
-	// "github.com/MasterGolang/Base/tiposDeDados"
-	// "github.com/MasterGolang/Base/variaveis"
-	// "github.com/MasterGolang/modules/math"
-)
+// // "github.com/MasterGolang/ferramentasFuncoes"
+// // "github.com/MasterGolang/ferramentasNativas"
+// // "github.com/MasterGolang/ferramentasTecnicas"
+// // "github.com/MasterGolang/ferramentasNativas"
+// // "github.com/MasterGolang/ferramentasTecnicas"
+// // "github.com/MasterGolang/funcoes"
+// // "github.com/MasterGolang/Base/agrupamento"
+// // "github.com/MasterGolang/Base/conversoes"
+// // "github.com/MasterGolang/Base/escritas"
+// // "github.com/MasterGolang/Base/fluxo"
+// // "github.com/MasterGolang/Base/loops"
+// // "github.com/MasterGolang/Base/objetos"
+// // "github.com/MasterGolang/Base/operadores"
+// // "github.com/MasterGolang/Base/tiposDeDados"
+// // "github.com/MasterGolang/Base/variaveis"
+// // "github.com/MasterGolang/modules/math"
+// )
 
 func main() {
 	// print("**** Hello Word - Main **** \n")
 
-	// ferramentasFuncoes.ExecuteDeferAdiarExecucao()
-	// ferramentasFuncoes.ExecuteAvaliacaoAlunoAdiando()
-	// ferramentasFuncoes.ExecutePanic()
-	// ferramentasFuncoes.ExecutePanicEstudando2()
-
 	// ferramentasTecnicas.PoloUnicoPonteiro()
-	// funcoes.RetornandoMaisQueUmaVar()
-	// funcoes.ReduceDeMultiplasEntradas()
+
 	// ferramentasNativas.VerificarTipos()
 	// ferramentasNativas.Textos()
 
-	// log.Println(base.Base())
 	// Base
+	// log.Println(base.Base())
+
 	// variaveis.Declaracoes()
+
 	// conversoes.StringParaNumero()
+
 	// escritas.PrintConsole()
+
 	// loops.Incrementar()
 	// loops.PrintarItems()
 	// loops.VariarEmString()
 	// loops.VariarPorObjetoMap()
+
 	// objetos.StructModeloEstrutura()
 	// objetos.ComposicaoEstrutura()
 	// objetos.ObjRigidoMap()
+
 	// operadores.Logicos()
+
 	// operadores.Unarios()
+
 	// operadores.Relacionais()
+
 	// tiposDeDados.Erro()
+
 	// tiposDeDados.NumFloat()
+
 	// tiposDeDados.NumInteiro()
+
 	// tiposDeDados.ValoresIniciais()
+
 	// agrupamento.ArrayFixo()
+
 	// agrupamento.SliceArrayLivre()
+
 	// fluxo.SwitchCase()
 
 	// log.Println(math.Soma(10, 100))
+
+	// funcoes.RetornandoMaisQueUmaVar()
+	// funcoes.ReduceDeMultiplasEntradas()
 	// funcoes.FuncoesAnonimas()
 	// funcoes.ExecuteClosure()
+	// ferramentasFuncoes.ExecuteDeferAdiarExecucao()
+	// ferramentasFuncoes.ExecuteAvaliacaoAlunoAdiando()
+	// ferramentasFuncoes.ExecutePanic()
+	// ferramentasFuncoes.ExecutePanicEstudando2()
 
 }
