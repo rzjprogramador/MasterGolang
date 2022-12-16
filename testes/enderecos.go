072b9bdac1f1cd4fd9f1fa08128e8dc32abe6985
rzjprogramador <rzjprogramador@gmail.com>
2022-12-16 14:52:11 -0300

    feat: pacote strings funcao split fatiando valor em colecao de strings

---
+++ testes/enderecos.go
@@ -0,0 +1,13 @@
+package enderecos
+
+import "strings"
+
+func tipoEnderecos(endereco string) string {
+	enderecosValidos := []string{"rua", "avenida", "logradouro"}
+
+	primeiraPalavraEndereco := strings.Split(endereco, " ")[0]
+}
+
+/*
+
+*/
